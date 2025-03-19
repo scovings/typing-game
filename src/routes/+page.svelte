4f68fc5cdb2a00b46cfbb00e2639ebc8c8910154
sco. <77859647+scovings@users.noreply.github.com>
2025-03-19 22:15:12 +0000

    err

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,59 @@
-<h1>sveltype</h1>
+<script lang="ts">
+  type Game = 'waiting for input' | 'in progress' | 'game over'
+  type Word = string
+
+  let game: Game = 'waiting for input'
+  let typedLetter = ''
+
+  let words: Word[] = "The quick brown fox jumps over the lazy dog".split(' ')
+  let wordIndex = 0
+  let letterIndex = 0
+  let correctLetters = 0
+
+  let wordsEl: HTMLDivElement
+  let letterEl: HTMLSpanElement
+  let inputEl: HTMLInputElement
+
+  function updateGameState() {}
+
+  function startGame() {
+    setGameState('in progress')
+  }
+
+  function setGameState(state: Game) {
+    game = state
+  }
+
+  function handleKeydown(event: KeyboardEvent) {
+    if (event.code === 'Space') {
+      event.preventDefault()
+    }
+
+    if (game === 'waiting for input') {
+      startGame()
+    }
+  }
+
+  function handleKeydown(event: KeyboardEvent) {}
+</script>
+
+<div class="game" data-game={game}>
+  <input
+    bind:this={inputEl}
+    bind:value={typedLetter}
+    on:input={updateGameState}
+    on:keydown={handleKeydown}
+    class="input"
+    type="text"
+  />
+
+  <div bind:this={wordsEl} class="words">
+    {#each words as word}
+      <span class="word">
+        {#each word as letter}
+          <span class="letter">{letter}</span>
+        {/each}
+      </span>
+    {/each}
+  </div>
+</div>
